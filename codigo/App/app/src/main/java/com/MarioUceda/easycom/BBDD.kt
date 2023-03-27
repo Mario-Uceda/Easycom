49d0b4033dc9ae98c4cb79821b778ac397c1dfa3
Mario Uceda <marioucedayeves@gmail.com>
2023-03-27 14:05:52 +0200

    Update BBDD.kt

--- codigo/App/app/src/main/java/com/MarioUceda/easycom/BBDD.kt
+++ codigo/App/app/src/main/java/com/MarioUceda/easycom/BBDD.kt
@@ -3,24 +3,27 @@ package com.MarioUceda.easycom
 import kotlinx.coroutines.*
 import okhttp3.*
 import okio.IOException
-import java.sql.Date
-import java.util.*
+import java.sql.*
+
 
 class BBDD {
 
     private val url = "https://mariouceda.es/classes/peticion.class.php"
     private val cliente = OkHttpClient()
+    //val fechaActual = Date(System.currentTimeMillis())
+
 
     fun registrarUsuario(email: String, username: String, password: String): Deferred<Boolean> = CoroutineScope(Dispatchers.IO).async {
         val registrado = CompletableDeferred<Boolean>()
-        val fecha_hoy = Date(Calendar.getInstance().timeInMillis)
+        val fechaActual = Date(System.currentTimeMillis())
+        println(fechaActual)
         val formBody = FormBody.Builder()
             .add("metodo", "registrarUsuario")
             .add("nombre", username)
             .add("email", email)
             .add("contraseña", password)
             .add("es_admin", "false")
-            .add("fecha_cracion", fecha_hoy.toString())
+            .add("fecha_creacion", fechaActual.toString())
             .build()
 
         val request = Request.Builder()
@@ -35,6 +38,8 @@ class BBDD {
 
             override fun onResponse(call: Call, respuesta: Response) {
                 if (respuesta.isSuccessful) {
+                    val respuestaBody = respuesta.body?.string()
+                    println(respuestaBody)
                     registrado.complete(true)
                 }else {
                     registrado.complete(false)
