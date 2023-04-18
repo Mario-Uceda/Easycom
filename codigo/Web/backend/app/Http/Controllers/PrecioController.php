55c5c89628c5c8b456007d9beed8a42ed5eaea38
unknown <marioucedayeves@gmail.com>
2023-04-18 19:27:49 +0200

    a

--- codigo/Web/backend/app/Http/Controllers/PrecioController.php
+++ codigo/Web/backend/app/Http/Controllers/PrecioController.php
@@ -9,22 +9,22 @@ class PrecioController extends Controller
     function actualizarPrecios()
     {
         $script_path = '../WebScraping/ActualizarPrecios.py';
-
-        $precios = Precio::select('id_producto', 'url_producto', 'tienda')->groupBy('id_producto', 'url_producto','tienda')->get();
-        $contador = 0;
+        $precios = Precio::select('id_producto', 'url_producto', 'tienda')
+        ->latest('created_at')
+        ->groupBy('id_producto', 'url_producto', 'tienda')
+        ->get();
         foreach ($precios as $precio_anterior) {
             $url = $precio_anterior->url_producto;
             $tienda = $precio_anterior->tienda;
             $id = $precio_anterior->id_producto;
             $command = 'python ' . $script_path . ' "' . $url . '" "' . $tienda . '" 2>&1';
+            $output = [];
             exec($command, $output);
-            $precio = $output[$contador];
-            $contador++;
+            $precio = $output[0];
             if (is_numeric($precio)) {
                 $precio_nuevo = new Precio($id, $precio, $tienda, $url);
                 $precio_nuevo->save();
             }
         }
     }
-
 }
