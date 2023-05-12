9797324ba08233b4931e554808292caa73c4ad01
unknown <marioucedayeves@gmail.com>
2023-05-12 13:24:32 +0200

    Update Notificacion.kt

--- codigo/App/app/src/main/java/com/MarioUceda/easycom/classes/Notificacion.kt
+++ codigo/App/app/src/main/java/com/MarioUceda/easycom/classes/Notificacion.kt
@@ -7,6 +7,6 @@ data class Notificacion (
     @SerializedName("id") var id: String,
     @SerializedName("id_producto") var idProducto: String,
     @SerializedName("precio_anterior") var precio_anterior: Double,
-    @SerializedName("precio_actual") var precio_actual: Double,
+    @SerializedName("precio_nuevo") var precio_actual: Double,
     @SerializedName("updated_at") var updated_at : String,
 ) : Serializable
